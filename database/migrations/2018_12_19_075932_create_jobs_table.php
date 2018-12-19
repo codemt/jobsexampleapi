fe70c84f2386a2f0e846c01d0184586164134f9d
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-12-19 14:16:06 +0530

    added cors and setup done for api server

---
+++ database/migrations/2018_12_19_075932_create_jobs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('job_title');
+            $table->string('job_company');
+            $table->string('job_profile');
+            $table->string('job_experience');
+            $table->string('job_salary');
+            $table->string('job_category');
+            $table->string('job_location');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
