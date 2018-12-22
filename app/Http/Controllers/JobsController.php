0f34651d3eecf6fca87013d1cfd200a1e6a03c83
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-12-22 22:13:31 +0530

    updated jobs API where id was not getting passed to API call

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Jobs;
 use App\JobDetails;
+
 class JobsController extends Controller
 {
-   
-        public function getAllJobs(){
+
+        public function getAllJobs()
+        {
 
 
                 $all = Jobs::all();
@@ -18,52 +20,56 @@ class JobsController extends Controller
 
         }
 
-        public function getIndividualJobs($id){
+        public function getIndividualJobs($id)
+        {
 
 
-                    $getJob = Jobs::find($id);
-                    return response()->json($getJob);
+                $getJob = Jobs::find($id);
+                return response()->json($getJob);
 
         }
 
-        public function getJobDetails($id){
+        public function getJobDetails($id)
+        {
 
 
-                $jobdetails = JobDetails::where('job_id',$id)->get();
+                $jobdetails = JobDetails::where('job_id', $id)->get();
                 //  $jobdata = json_encode(array_merge(json_decode($getJob, true),json_decode($jobdetails, true)));
                 return response()->json($jobdetails);
 
         }
 
-        public function getJobByCategory($category){
+        public function getJobByCategory($category)
+        {
 
 
 
 
                     //return $category;
-                    $jobs = Jobs::where('job_category',$category)->get();
+                $jobs = Jobs::where('job_category', $category)->get();
 
-                    return response()->json($jobs); 
+                return response()->json($jobs);
         }
 
 
-        public function getJobBySearch($location,$experience,$jobtype){
+        public function getJobBySearch($location, $experience, $jobtype)
+        {
 
 
-                
-
-            $jobs = Jobs::select('job_title','job_company','job_profile','job_experience','job_salary','job_location')
-                    ->where('job_location',$location)
-                    ->where('job_experience',$experience)
-                    ->where('job_type',$jobtype)
-                    ->get();
 
 
-                    return response()->json($jobs);
+                $jobs = Jobs::select('id','job_title', 'job_company', 'job_profile', 'job_experience', 'job_salary', 'job_location')
+                        ->where('job_location', $location)
+                        ->where('job_experience', $experience)
+                        ->where('job_type', $jobtype)
+                        ->get();
+
+
+                return response()->json($jobs);
 
 
         }
 
-      
+
 }
 
